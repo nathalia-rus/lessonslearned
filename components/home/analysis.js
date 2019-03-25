2945c907fa24c7081f401cb62f74d65bd71ac320
Nathalia Rus <nathaliarus@live.fr>
2019-03-25 16:15:36 +0000

    ok

--- components/home/analysis.js
+++ components/home/analysis.js
@@ -18,7 +18,6 @@ class Analysis extends React.Component {
     super(props);
     this.state = {
       doc: null,
-      analyses: data,
       descriptionShown: false,
       added: false
     };
@@ -28,9 +27,6 @@ class Analysis extends React.Component {
 
   componentDidMount() {
     const apiEndpoint = "https://lessonslearned.prismic.io/api/v2";
-    // or http://lessonslearned.prismic.io/api/v2/documents/search?ref=XJjwjhAAAAVodNpn
-    // XJjwjhAAAAVodNpn being my master red, returning 4 documents
-    // actually no -set to JSON and ok returned
 
     Prismic.api(apiEndpoint).then(api => {
       api
@@ -52,144 +48,153 @@ class Analysis extends React.Component {
   toggleAdded() {
     this.setState({ added: !this.state.added });
   }
-
+  //  this.state.doc != null && this.state.doc[0].data
   render() {
-    return this.state.analyses.map((analysis, index) => {
-      return (
-        <div className="black avenir  measure lh-copy " key={index}>
-          <div className=" f4 mt3 pt2 mb1 measure-narrow mr4 fw5  ">
-            {analysis.title}
-          </div>
-          {console.log(this.state.doc != null && this.state.doc[0].data)}
-          <div className="lh-copy measure-narrow ">
-            <div className="garamond mr4"> {analysis.intro}</div>
-            <button
-              className="f2 btn link"
-              onClick={e => {
-                e.preventDefault();
-                this.toggleDescriptionShown();
-              }}
-            >
-              {this.state.descriptionShown ? (
-                <div className="tl f5 garamond lh-copy measure-narrow mt2 fw1 i">
-                  {analysis.description}
-                  <Octicon
-                    icon={ChevronUp}
-                    size="small"
-                    className="ph1 black "
-                  />
-                </div>
-              ) : (
-                "..."
-              )}
-            </button>
-            <div className="footer-wrapper mt2">
-              <div className="author  avenir">
-                <div className="fw5 navy"> {analysis.author}</div>
-                <div className="f6"> {analysis.position} </div>
-                <div className="basket">
-                  <div className="f7 mt2 centered pt1"> £9.99 </div>
-                  {this.state.added ? (
-                    <button
-                      className="btn link"
-                      onClick={e => {
-                        e.preventDefault();
-                        this.toggleAdded();
-                      }}
-                    >
-                      <div className="add-round ml2 mt2 centered green">
-                        <Octicon
-                          icon={Check}
-                          size="small"
-                          className="ph1 black-70 "
-                        />
-                      </div>
-                    </button>
-                  ) : (
-                    <button
-                      className="btn link"
-                      onClick={e => {
-                        e.preventDefault();
-                        this.toggleAdded();
-                      }}
-                    >
-                      <div className=" add-round ml2 mt2 centered black-70">
-                        <Octicon
-                          icon={PlusSmall}
-                          size="small"
-                          className="ph1 black-70 "
-                        />
-                      </div>
-                    </button>
-                  )}
-                </div>
-              </div>
-              <div className="footer mr4 mt3 i measure-narrow ">
-                <div className="f7 grey fw3 tr ">
-                  <Octicon icon={Eye} size="small" className="ph1 black-70" />
-                  {analysis.views}
-                  <Octicon icon={Heart} size="small" className="ph1 black-70" />
-                  {analysis.likes}
-                </div>
-                <div className="f7 tr fw3 grey ">{analysis.keywords}</div>
-              </div>
+    return (
+      this.state.doc != null &&
+      this.state.doc.map((analysis, index) => {
+        return (
+          <div className="black avenir  measure lh-copy " key={index}>
+            <div className=" f4 mt3 pt2 mb1 measure-narrow mr4 fw5  ">
+              {analysis.data.title}
             </div>
-            <div className="line mt4 mb4 black-40" />
+
+            <div className="lh-copy measure-narrow ">
+              <div className="garamond mr4"> {analysis.data.intro}</div>
+              <button
+                className="f2 btn link"
+                onClick={e => {
+                  e.preventDefault();
+                  this.toggleDescriptionShown();
+                }}
+              >
+                {this.state.descriptionShown ? (
+                  <div className="tl f5 garamond lh-copy measure-narrow mt2 fw1 i">
+                    {analysis.data.description}
+                    <Octicon
+                      icon={ChevronUp}
+                      size="small"
+                      className="ph1 black "
+                    />
+                  </div>
+                ) : (
+                  "..."
+                )}
+              </button>
+              <div className="footer-wrapper mt2">
+                <div className="author  avenir">
+                  <div className="fw5 navy"> {analysis.data.author}</div>
+                  <div className="f6"> {analysis.data.position} </div>
+                  <div className="basket">
+                    <div className="f7 mt2 centered pt1"> £9.99 </div>
+                    {this.state.added ? (
+                      <button
+                        className="btn link"
+                        onClick={e => {
+                          e.preventDefault();
+                          this.toggleAdded();
+                        }}
+                      >
+                        <div className="add-round ml2 mt2 centered green">
+                          <Octicon
+                            icon={Check}
+                            size="small"
+                            className="ph1 black-70 "
+                          />
+                        </div>
+                      </button>
+                    ) : (
+                      <button
+                        className="btn link"
+                        onClick={e => {
+                          e.preventDefault();
+                          this.toggleAdded();
+                        }}
+                      >
+                        <div className=" add-round ml2 mt2 centered black-70">
+                          <Octicon
+                            icon={PlusSmall}
+                            size="small"
+                            className="ph1 black-70 "
+                          />
+                        </div>
+                      </button>
+                    )}
+                  </div>
+                </div>
+                <div className="footer mr4 mt3 i measure-narrow ">
+                  <div className="f7 grey fw3 tr ">
+                    <Octicon icon={Eye} size="small" className="ph1 black-70" />
+                    {analysis.data.views}
+                    <Octicon
+                      icon={Heart}
+                      size="small"
+                      className="ph1 black-70"
+                    />
+                    {analysis.data.likes}
+                  </div>
+                  <div className="f7 tr fw3 grey ">
+                    {analysis.data.keywords}
+                  </div>
+                </div>
+              </div>
+              <div className="line mt4 mb4 black-40" />
+            </div>
+            <style jsx>
+              {`
+                .basket {
+                  display: flex;
+                  flex-direction: row;
+                }
+
+                .footer-wrapper {
+                  display: flex;
+                  flex-direction: row;
+                  justify-content: space-between;
+                }
+
+                .footer {
+                  align-self: flex-start;
+                }
+                .centered {
+                  align-self: center;
+                }
+
+                .author {
+                  align-self: center;
+                  width: 70%;
+                }
+                .line {
+                  width: 80vw;
+                  height: 1px;
+                  border-bottom: 1px dotted;
+                }
+
+                .add-round {
+                  height: 20px;
+                  width: 20px;
+                  border-radius: 50%;
+                  border: solid 1px;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                }
+                .btn {
+                  border: none;
+                  background: transparent;
+                  margin-left: -5px;
+                }
+
+                .link:focus {
+                  transition: color 0.15s ease-in;
+                  outline: 1px none;
+                }
+              `}
+            </style>
           </div>
-          <style jsx>
-            {`
-              .basket {
-                display: flex;
-                flex-direction: row;
-              }
-
-              .footer-wrapper {
-                display: flex;
-                flex-direction: row;
-                justify-content: space-between;
-              }
-
-              .footer {
-                align-self: flex-start;
-              }
-              .centered {
-                align-self: center;
-              }
-
-              .author {
-                align-self: center;
-                width: 70%;
-              }
-              .line {
-                width: 80vw;
-                height: 1px;
-                border-bottom: 1px dotted;
-              }
-
-              .add-round {
-                height: 20px;
-                width: 20px;
-                border-radius: 50%;
-                border: solid 1px;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-              }
-              .btn {
-                border: none;
-                background: transparent;
-                margin-left: -5px;
-              }
-
-              .link:focus {
-                transition: color 0.15s ease-in;
-                outline: 1px none;
-              }
-            `}
-          </style>
-        </div>
-      );
-    });
+        );
+      })
+    );
   }
 }
 
